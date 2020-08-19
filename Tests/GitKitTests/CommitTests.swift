a2ad0a9afdf5509b0e8ee7cc6e9b488df804def3
Daniel Tull <dt@danieltull.co.uk>
2020-08-19 23:41:36 +0100

    Add test for parentIDs

--- Tests/GitKitTests/CommitTests.swift
+++ Tests/GitKitTests/CommitTests.swift
@@ -25,10 +25,13 @@ final class CommitTests: XCTestCase {
             XCTAssertEqual(last.committer.email, "dt@danieltull.co.uk")
             XCTAssertEqual(last.committer.date, Date(timeIntervalSince1970: 1595183161))
             XCTAssertEqual(last.committer.timeZone, TimeZone(secondsFromGMT: 3600))
-            let first = try XCTUnwrap(commits.first)
-            XCTAssertEqual(main.objectID, first.id.rawValue)
             XCTAssertEqual(try last.parents().count, 0)
             XCTAssertEqual(last.parentIDs.count, 0)
+            let first = try XCTUnwrap(commits.first)
+            XCTAssertEqual(main.objectID, first.id.rawValue)
+            XCTAssertEqual(first.parentIDs.count, 2)
+            XCTAssertEqual(try first.parentIDs.value(at: 0).description, "17e26bc76cff375603e7173dac31e5183350e559")
+            XCTAssertEqual(try first.parentIDs.value(at: 1).description, "c8b08c2ed176eaaf7cea877f774319a27684870a")
         }
     }
 
