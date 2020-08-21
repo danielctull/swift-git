ec11e86417a1a5ecd66ee2d889d38316050227a5
Daniel Tull <dt@danieltull.co.uk>
2020-08-21 13:21:34 +0100

    Move the repository remote branch functions to their own file

---
+++ Sources/GitKit/Repository+RemoteBranch.swift
@@ -0,0 +1,26 @@
+
+import Clibgit2
+
+extension Repository {
+
+    public func remoteBranches() throws -> [RemoteBranch] {
+
+        try GitIterator(
+            createIterator: { git_branch_iterator_new($0, repository.pointer, GIT_BRANCH_REMOTE) },
+            freeIterator: git_branch_iterator_free,
+            nextElement: {
+                let type = UnsafeMutablePointer<git_branch_t>.allocate(capacity: 1)
+                defer { type.deallocate() }
+                return git_branch_next($0, type, $1)
+            },
+            freeElement: git_reference_free)
+            .map(RemoteBranch.init)
+    }
+
+    public func remoteBranch(named name: String) throws -> RemoteBranch {
+        let pointer = try GitPointer(
+            create: { git_branch_lookup($0, repository.pointer, name, GIT_BRANCH_REMOTE) },
+            free: git_reference_free)
+        return try RemoteBranch(pointer)
+    }
+}
