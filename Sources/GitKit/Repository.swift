cd48eb83edb39ce751085e2902727743c93cc172
Daniel Tull <dt@danieltull.co.uk>
2020-08-21 13:10:10 +0100

    Move the repository commit functions to their own file

--- Sources/GitKit/Repository.swift
+++ Sources/GitKit/Repository.swift
@@ -151,67 +151,6 @@ extension Repository {
     }
 }
 
-// MARK: - Commits
-
-public struct SortOptions: OptionSet {
-    public let rawValue: UInt32
-    public init(rawValue: UInt32) {
-        self.rawValue = rawValue
-    }
-    private init(_ sort: git_sort_t) {
-        self.init(rawValue: sort.rawValue)
-    }
-    public init() { self.init(GIT_SORT_NONE) }
-    public static let time = Self(GIT_SORT_TIME)
-    public static let topological = Self(GIT_SORT_TOPOLOGICAL)
-    public static let reverse = Self(GIT_SORT_REVERSE)
-}
-
-extension Repository {
-
-    public func commits(
-        for references: Reference...,
-        sortedBy sortOptions: SortOptions = SortOptions(),
-        includeHead: Bool = true
-    ) throws -> [Commit] {
-        try commits(for: references,
-                    sortedBy: sortOptions,
-                    includeHead: includeHead)
-    }
-
-    public func commits(
-        for references: [Reference] = [],
-        sortedBy sortOptions: SortOptions = SortOptions(),
-        includeHead: Bool = true
-    ) throws -> [Commit] {
-
-        try GitIterator(
-            createIterator: { git_revwalk_new($0, repository.pointer) },
-            configureIterator: { iterator in
-                for reference in references {
-                    var oid = reference.target.oid
-                    let result = git_revwalk_push(iterator, &oid)
-                    if LibGit2Error(result) != nil { return result }
-                }
-                if includeHead {
-                    let result = git_revwalk_push_head(iterator)
-                    if LibGit2Error(result) != nil { return result }
-                }
-                return git_revwalk_sorting(iterator, sortOptions.rawValue)
-            },
-            freeIterator: git_revwalk_free,
-            nextElement: { commit, iterator in
-                let oid = UnsafeMutablePointer<git_oid>.allocate(capacity: 1)
-                defer { oid.deallocate() }
-                let result = git_revwalk_next(oid, iterator)
-                if LibGit2Error(result) != nil { return result }
-                return git_commit_lookup(commit, repository.pointer, oid)
-            },
-            freeElement: git_commit_free)
-            .map(Commit.init)
-    }
-}
-
 // MARK: - Reflog
 
 extension Repository {
