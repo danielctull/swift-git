c940193314fa3f0474bb6e892dcd415ec7932538
Daniel Tull <dt@danieltull.co.uk>
2021-11-18 16:26:59 +0000

    Make Repository.remoteBranches a throwing property

--- Sources/GitKit/Repository+RemoteBranch.swift
+++ Sources/GitKit/Repository+RemoteBranch.swift
@@ -3,18 +3,19 @@ import Clibgit2
 
 extension Repository {
 
-    public func remoteBranches() throws -> [RemoteBranch] {
-
-        try GitIterator(
-            createIterator: repository.create(git_branch_iterator_new, GIT_BRANCH_REMOTE),
-            freeIterator: git_branch_iterator_free,
-            nextElement: {
-                let type = UnsafeMutablePointer<git_branch_t>.allocate(capacity: 1)
-                defer { type.deallocate() }
-                return git_branch_next($0, type, $1)
-            },
-            freeElement: git_reference_free)
-            .map(RemoteBranch.init)
+    public var remoteBranches: [RemoteBranch] {
+        get throws {
+            try GitIterator(
+                createIterator: repository.create(git_branch_iterator_new, GIT_BRANCH_REMOTE),
+                freeIterator: git_branch_iterator_free,
+                nextElement: {
+                    let type = UnsafeMutablePointer<git_branch_t>.allocate(capacity: 1)
+                    defer { type.deallocate() }
+                    return git_branch_next($0, type, $1)
+                },
+                freeElement: git_reference_free)
+                .map(RemoteBranch.init)
+        }
     }
 
     public func remoteBranch(on remote: Remote.ID, named branch: String) throws -> RemoteBranch {
