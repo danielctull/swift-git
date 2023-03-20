28b7c184415b681411e0be2e46603df07c06d308
Daniel Tull <dt@danieltull.co.uk>
2023-03-20 17:59:40 +0000

    Add Index

---
+++ Sources/Git/Index.swift
@@ -0,0 +1,44 @@
+
+import Clibgit2
+
+public struct Index: GitReference {
+    let pointer: GitPointer
+}
+
+extension Repository {
+
+    public var index: Index {
+        get throws {
+            try Index(
+                create: pointer.get(git_repository_index),
+                free: git_index_free)
+        }
+    }
+}
+
+extension Index {
+
+    public struct Entry {
+        public let objectID: Object.ID
+    }
+
+    public var entries: [Entry] {
+        get throws {
+            try GitCollection(
+                pointer: pointer,
+                count: git_index_entrycount,
+                element: git_index_get_byindex
+            )
+            .map(Unwrap)
+            .map(\.pointee)
+            .map(Entry.init)
+        }
+    }
+}
+
+extension Index.Entry {
+
+    fileprivate init(_ entry: git_index_entry) {
+        objectID = Object.ID(oid: entry.id)
+    }
+}
