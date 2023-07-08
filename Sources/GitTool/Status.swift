1c1f708d0b7b4efbee295c3c0fbd7ac04555ef3b
Daniel Tull <dt@danieltull.co.uk>
2023-07-08 15:18:13 +0100

    Improvement to the status code

--- Sources/GitTool/Status.swift
+++ Sources/GitTool/Status.swift
@@ -4,6 +4,11 @@ import Git
 
 struct NoCurrentDirectoryURL: Error {}
 
+extension Diff.Delta {
+
+  var file: Diff.File? { to ?? from }
+}
+
 struct Status: ParsableCommand {
 
   static let configuration = CommandConfiguration(commandName: "status")
@@ -25,52 +30,57 @@ struct Status: ParsableCommand {
 
     let entries = try repo.status
 
-    let headToIndex = entries.filter({ $0.headToIndex != nil })
-    if !headToIndex.isEmpty {
-      print("")
-      print("Changes to be committed:")
-      for entry in headToIndex {
-        let delta = entry.headToIndex!
-        guard let file = delta.to ?? delta.from else { continue }
+    let index: [(String, Diff.File)] = entries.compactMap { entry in
 
-        if entry.status.contains(.indexNew) {
-          print("        new file:   " + file.path)
-        } else if entry.status.contains(.indexDeleted) {
-          print("        deleted:    " + file.path)
-        } else if entry.status.contains(.indexRenamed) {
-          print("        renamed:    " + file.path)
-        } else if entry.status.contains(.indexModified) {
-          print("        modified:   " + file.path)
-        } else if entry.status.contains(.indexTypeChange) {
-          print("        type change:" + file.path)
+      guard let file = entry.headToIndex?.file else { return nil }
+
+      if entry.status.contains(.indexNew) {
+        return ("new file:   ", file)
+      } else if entry.status.contains(.indexDeleted) {
+        return ("deleted:    ", file)
+      } else if entry.status.contains(.indexRenamed) {
+        return ("renamed:    ", file)
+      } else if entry.status.contains(.indexModified) {
+        return ("modified:   ", file)
+      } else if entry.status.contains(.indexTypeChange) {
+        return ("type change:", file)
+      } else {
+        return nil
+      }
+    }
+
+    let workingDirectory: [(String, Diff.File)] = entries.compactMap { entry in
+
+      guard let file = entry.indexToWorkingDirectory?.file else { return nil }
+
+      if entry.status.contains(.workingTreeNew) {
+        return ("new file:   ", file)
+      } else if entry.status.contains(.workingTreeDeleted) {
+        return ("deleted:    ", file)
+      } else if entry.status.contains(.workingTreeRenamed) {
+        return ("renamed:    ", file)
+      } else if entry.status.contains(.workingTreeModified) {
+        return ("modified:   ", file)
+      } else if entry.status.contains(.workingTreeUnreadable) {
+        return ("unreadable: ", file)
+      } else if entry.status.contains(.workingTreeTypeChange) {
+        return ("type change:", file)
+      } else {
+        return nil
+      }
+    }
+
+    func printFiles(_ title: String, files: [(String, Diff.File)]) {
+      if !files.isEmpty {
+        print("")
+        print(title)
+        for entry in files {
+          print("        \(entry.0)\(entry.1.path)")
         }
       }
     }
 
-    let indexToWorkingDirectory = entries.filter({
-      $0.indexToWorkingDirectory != nil
-    })
-    if !indexToWorkingDirectory.isEmpty {
-      print("")
-      print("Changes not staged for commit:")
-      for entry in indexToWorkingDirectory {
-        let delta = entry.indexToWorkingDirectory!
-        guard let file = delta.to ?? delta.from else { continue }
-
-        if entry.status.contains(.workingTreeNew) {
-          print("        new file:   " + file.path)
-        } else if entry.status.contains(.workingTreeDeleted) {
-          print("        deleted:    " + file.path)
-        } else if entry.status.contains(.workingTreeRenamed) {
-          print("        renamed:    " + file.path)
-        } else if entry.status.contains(.workingTreeModified) {
-          print("        modified:   " + file.path)
-        } else if entry.status.contains(.workingTreeUnreadable) {
-          print("        unreadable: " + file.path)
-        } else if entry.status.contains(.workingTreeTypeChange) {
-          print("        type change:" + file.path)
-        }
-      }
-    }
+    printFiles("Changes to be committed:", files: index)
+    printFiles("Changes not staged for commit:", files: workingDirectory)
   }
 }
