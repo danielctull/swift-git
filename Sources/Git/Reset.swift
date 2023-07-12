e42ab80e3db1e20bc160c3800ab9a030a4b94cbc
Daniel Tull <dt@danieltull.co.uk>
2023-07-12 08:46:06 +0100

    Add documentation for reset

--- Sources/Git/Reset.swift
+++ Sources/Git/Reset.swift
@@ -4,6 +4,29 @@ import Tagged
 
 extension Repository {
 
+    /// Sets the current head to the specified commit oid and optionally
+    /// resets the index and working tree to match.
+    ///
+    /// * ``soft``: the Head will be moved to the commit.
+    ///
+    /// * ``mixed``: trigger a soft reset, plus the index will be replaced
+    /// with the content of the commit tree.
+    ///
+    /// * ``hard``: trigger a mixed reset and the working directory will be
+    /// replaced with the content of the index. (Untracked and ignored files
+    /// will be left alone, however.)
+    ///
+    /// - Parameters:
+    ///   - commitish: Committish to which the Head should be moved to. This
+    ///                object must belong to the given `repo` and can either be
+    ///                a ``Commit`` or a ``Tag``. When a tag is being passed,
+    ///                it should be dereferenceable to a commit which oid will
+    ///                be used as the target of the branch.
+    ///   - operation: Kind of reset operation to perform.
+    ///   - checkoutOptions: Optional checkout options to be used for a hard
+    ///                      reset. The checkout_strategy field will be
+    ///                      overridden (based on operation). This parameter can
+    ///                      be used to propagate notify and progress callbacks.
     @GitActor
     public func reset(
         to commitish: Commitish,
