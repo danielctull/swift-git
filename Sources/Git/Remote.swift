b8e447dc1badf3342c9325d76e18e501907040ba
Daniel Tull <dt@danieltull.co.uk>
2023-07-14 09:53:51 +0100

    Add Remote.Name type

--- Sources/Git/Remote.swift
+++ Sources/Git/Remote.swift
@@ -6,8 +6,8 @@ import Tagged
 extension Repository {
 
     @GitActor
-    public func remote(for id: Remote.ID) throws -> Remote {
-        try id.rawValue.withCString { id in
+    public func remote(named name: Remote.Name) throws -> Remote {
+        try name.rawValue.withCString { id in
             try Remote(
                 create: pointer.create(git_remote_lookup, id),
                 free: git_remote_free)
@@ -22,6 +22,7 @@ public struct Remote: Equatable, Hashable, Identifiable, Sendable {
     let pointer: GitPointer
     public typealias ID = Tagged<Remote, String>
     public let id: ID
+    public let name: Name
 //    public let url: URL
 
     @GitActor
@@ -29,14 +30,32 @@ public struct Remote: Equatable, Hashable, Identifiable, Sendable {
         self.pointer = pointer
         let name = try pointer.get(git_remote_name) |> String.init
         id = ID(name)
+        self.name = Name(rawValue: name)
 
 //        let urlString = try Unwrap(String(remote.get(git_remote_url)))
 //        url = try Unwrap(URL(string: urlString))
     }
 }
 
+// MARK: - Remote.Name
+
 extension Remote {
-    public var name: String { id.rawValue }
+
+    public struct Name: Equatable, Hashable, Sendable {
+        let rawValue: String
+    }
+}
+
+extension Remote.Name: ExpressibleByStringLiteral {
+
+    public init(stringLiteral value: String) {
+        self.init(rawValue: value)
+    }
+}
+
+extension Remote.Name: CustomStringConvertible {
+
+    public var description: String { rawValue }
 }
 
 // MARK: - GitPointerInitialization
--- Tests/GitTests/RemoteTests.swift
+++ Tests/GitTests/RemoteTests.swift
@@ -10,7 +10,7 @@ final class RemoteTests: XCTestCase {
         let remote = try Bundle.module.url(forRepository: "Test.git")
         try FileManager.default.withTemporaryDirectory { local in
             let repo = try Repository(local: local, remote: remote)
-            let remote = try repo.remote(for: "origin")
+            let remote = try repo.remote(named: "origin")
             XCTAssertEqual(remote.id, "origin")
             XCTAssertEqual(remote.name, "origin")
 //            XCTAssertEqual(remote.url, local)
