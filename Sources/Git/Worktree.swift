2dfd1a5e6adfa3af9ed5008fd9efcf04f0d996d4
Daniel Tull <dt@danieltull.co.uk>
2026-01-23 16:58:47 +0000

    Add the ability to add a working tree

--- Sources/Git/Worktree.swift
+++ Sources/Git/Worktree.swift
@@ -19,11 +19,78 @@ extension Repository {
       }
     }
   }
+
+  /// Add a new working tree for the repository at the given path.
+  ///
+  /// If a reference is provided, it will be checked out in the new working
+  /// tree.
+  public func addWorktree(
+    named name: Worktree.Name,
+    at path: URL,
+    reference: Reference? = nil
+  ) throws -> Worktree {
+    try name.withCString { name in
+      try path.withUnsafeFileSystemRepresentation { path in
+
+        var options = git_worktree_add_options()
+
+        git_worktree_add_options_init(
+          &options,
+          UInt32(GIT_WORKTREE_ADD_OPTIONS_VERSION)
+        )
+
+        if let reference {
+          options.ref = reference.pointer.pointer
+        }
+
+        return try withUnsafePointer(to: options) { options in
+          try Worktree(
+            pointer: Managed(
+              create: pointer.create(git_worktree_add, name, path, options),
+              free: git_worktree_free
+            )
+          )
+        }
+      }
+    }
+  }
 }
 
 // MARK: - Worktree
 
-public enum Worktree {}
+public struct Worktree: Equatable, Hashable, Identifiable {
+
+  let pointer: Managed<OpaquePointer>
+  public let id: ID
+  public let name: Name
+  public let path: URL
+
+  init(pointer: Managed<OpaquePointer>) throws {
+    self.pointer = pointer
+    name = try pointer.get(git_worktree_name)
+      |> Unwrap
+      |> String.init(cString:)
+      |> Name.init
+    id = ID(name: name)
+    path = try pointer.get(git_worktree_path)
+      |> Unwrap
+      |> String.init(cString:)
+      |> URL.init(fileURLWithPath:)
+  }
+}
+
+// MARK: - Worktree.ID
+
+extension Worktree {
+
+  public struct ID: Equatable, Hashable, Sendable {
+    fileprivate let name: Name
+  }
+}
+
+extension Worktree.ID: CustomStringConvertible {
+  public var description: String { name.description }
+}
 
 // MARK: - Worktree.Name
 
--- Tests/GitTests/WorktreeTests.swift
+++ Tests/GitTests/WorktreeTests.swift
@@ -10,4 +10,49 @@ struct WorktreeTests {
     let repository = try Repository.clone(.repository, to: .scratchDirectory)
     try #expect(repository.worktrees.isEmpty)
   }
+
+  @Test(.scratchDirectory(.random), .repositoryURL("Test.git"))
+  func addWorktree() throws {
+    let repository = try Repository.clone(.repository, to: .scratchDirectory)
+    let path = URL.scratchDirectory.appending(path: "path")
+
+    let worktree = try repository.addWorktree(
+      named: "test",
+      at: path
+    )
+
+    #expect(worktree.name == "test")
+    #expect(worktree.path.resolvingSymlinksInPath().path == path.resolvingSymlinksInPath().path)
+  }
+
+  @Test(.scratchDirectory(.random), .repositoryURL("Test.git"))
+  func addWorktreeAtReference() throws {
+    let repository = try Repository.clone(.repository, to: .scratchDirectory)
+    let main = try repository.branch(named: "main")
+    let commits = try Array(repository.commits(for: .branch(main)))
+    let commit = try #require(commits.first)
+    let new = try repository.createBranch(named: "new", at: commit)
+    let path = URL.scratchDirectory.appending(path: "path")
+
+    let worktree = try repository.addWorktree(
+      named: "test",
+      at: path,
+      reference: .branch(new)
+    )
+
+    #expect(worktree.name == "test")
+    #expect(worktree.path.resolvingSymlinksInPath().path == path.resolvingSymlinksInPath().path)
+  }
+
+  @Test(.scratchDirectory(.random), .repositoryURL("Test.git"))
+  func worktrees() throws {
+    let repository = try Repository.clone(.repository, to: .scratchDirectory)
+    let a = URL.scratchDirectory.appending(path: "a")
+    let b = URL.scratchDirectory.appending(path: "b")
+
+    _ = try repository.addWorktree(named: "b", at: b)
+    _ = try repository.addWorktree(named: "a", at: a)
+
+    try #expect(Array(repository.worktrees) == ["a", "b"])
+  }
 }
