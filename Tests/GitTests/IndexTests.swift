94928b2f7b9449c2aa85bd180205bfd46da645e1
Daniel Tull <dt@danieltull.co.uk>
2026-02-01 12:23:58 +0000

    Fix: Don't rely on platform-specific ordering

--- Tests/GitTests/IndexTests.swift
+++ Tests/GitTests/IndexTests.swift
@@ -11,20 +11,11 @@ struct IndexTests {
     let entries = try Array(repository.index.entries)
     #expect(entries.count == 2)
 
-    do {
-      let entry = try entries.value(at: 0)
-      #expect(
-        entry.objectID.description
-          == "96c36b4c2da3a3b8472d437cea0497d38f125b04"
-      )
-    }
-
-    do {
-      let entry = try entries.value(at: 1)
-      #expect(
-        entry.objectID.description
-          == "e5c0a8638a0d8dfa0c733f9d666c511f7e1f9a96"
-      )
-    }
+    #expect(
+      Set(entries.map(\.objectID.description)) == [
+        "96c36b4c2da3a3b8472d437cea0497d38f125b04",
+        "e5c0a8638a0d8dfa0c733f9d666c511f7e1f9a96",
+      ]
+    )
   }
 }
